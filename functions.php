e6a57913d2e7bf8d9f5efc31bd66604556280cad
Eric Bai <eric@ericbai.com>
2024-03-10 13:11:10 -0400

    added shared constants to the theme to avoid breaking if plugin is deactivate first

--- functions.php
+++ functions.php
@@ -1,8 +1,83 @@
 <?php
 
-// ***********************************************************
-// * SEE DFH PLUGIN `docsforhealth.php` FOR SHARED CONSTANTS *
-// ***********************************************************
+// ********************************
+// * Shared constants with plugin *
+// ********************************
+
+if (!defined('DFH_TEXT_DOMAIN')) {
+    define('DFH_TEXT_DOMAIN', 'dfh');
+}
+
+// Block categories
+if (!defined('DFH_BLOCK_CATEGORY_COMMON')) {
+    define('DFH_BLOCK_CATEGORY_COMMON', 'dfh-common');
+}
+if (!defined('DFH_BLOCK_CATEGORY_DEPRECATED')) {
+    define('DFH_BLOCK_CATEGORY_DEPRECATED', 'dfh-deprecated');
+}
+if (!defined('DFH_BLOCK_CATEGORY_LANDING')) {
+    define('DFH_BLOCK_CATEGORY_LANDING', 'dfh-landing');
+}
+if (!defined('DFH_BLOCK_CATEGORY_LAYOUT')) {
+    define('DFH_BLOCK_CATEGORY_LAYOUT', 'dfh-layout');
+}
+if (!defined('DFH_BLOCK_CATEGORY_MEDIA')) {
+    define('DFH_BLOCK_CATEGORY_MEDIA', 'dfh-media');
+}
+if (!defined('DFH_BLOCK_CATEGORY_CUSTOM_DATA')) {
+    define('DFH_BLOCK_CATEGORY_CUSTOM_DATA', 'dfh-custom-data');
+}
+
+// Content types
+if (!defined('DFH_CONTENT_TYPE_RESOURCE')) {
+    define('DFH_CONTENT_TYPE_RESOURCE', 'dfh_resource');
+}
+if (!defined('DFH_CONTENT_TYPE_TOOLKIT')) {
+    define('DFH_CONTENT_TYPE_TOOLKIT', 'dfh_toolkit');
+}
+if (!defined('DFH_CONTENT_TYPE_DOT_PHRASE')) {
+    define('DFH_CONTENT_TYPE_DOT_PHRASE', 'dfh_dot_phrase');
+}
+
+// Template block for each content type
+if (!defined('DFH_TEMPLATE_BLOCK_PAGE')) {
+    define('DFH_TEMPLATE_BLOCK_PAGE', 'dfh/new-page-placeholder');
+}
+if (!defined('DFH_TEMPLATE_BLOCK_RESOURCE')) {
+    define('DFH_TEMPLATE_BLOCK_RESOURCE', 'dfh/resource-detail');
+}
+if (!defined('DFH_TEMPLATE_BLOCK_TOOLKIT')) {
+    define('DFH_TEMPLATE_BLOCK_TOOLKIT', 'dfh/toolkit-detail');
+}
+if (!defined('DFH_TEMPLATE_BLOCK_DOT_PHRASE')) {
+    define('DFH_TEMPLATE_BLOCK_DOT_PHRASE', 'dfh/dot-phrase-detail');
+}
+
+// Required block for each content type
+if (!defined('DFH_REQUIRED_BLOCK_RESOURCE')) {
+    define('DFH_REQUIRED_BLOCK_RESOURCE', 'dfh/resource-detail-description');
+}
+if (!defined('DFH_REQUIRED_BLOCK_TOOLKIT')) {
+    define('DFH_REQUIRED_BLOCK_TOOLKIT', 'dfh/toolkit-detail-metadata');
+}
+if (!defined('DFH_REQUIRED_BLOCK_DOT_PHRASE')) {
+    define('DFH_REQUIRED_BLOCK_DOT_PHRASE', 'dfh/content-copy-area');
+}
+
+// Taxonomies
+if (!defined('DFH_TAXONOMY_RESOURCE')) {
+    define('DFH_TAXONOMY_RESOURCE', 'dfh_resource_classification');
+}
+if (!defined('DFH_TAXONOMY_RESOURCE_TYPE')) {
+    define('DFH_TAXONOMY_RESOURCE_TYPE', 'dfh_resource_content_type');
+}
+if (!defined('DFH_TAXONOMY_DOT_PHRASE_CATEGORY')) {
+    define('DFH_TAXONOMY_DOT_PHRASE_CATEGORY', 'dfh_dot_phrase_category');
+}
+
+// ****************************
+// * Theme-specific constants *
+// ****************************
 
 if (!defined('DFH_MENU_HEADER')) {
     define('DFH_MENU_HEADER', 'dfh-menu-nav');
